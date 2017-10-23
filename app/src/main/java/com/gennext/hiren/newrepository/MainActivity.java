d1469a1df03f9f845c6e6fc0bac3c17ef6823b4f
hiren <patelhiren3535@gmail.com>
2017-10-23 17:30:53 +0530

    new commit

--- app/src/main/java/com/gennext/hiren/newrepository/MainActivity.java
+++ app/src/main/java/com/gennext/hiren/newrepository/MainActivity.java
@@ -1,13 +1,35 @@
-    package com.gennext.hiren.newrepository;
+package com.gennext.hiren.newrepository;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.util.ArrayMap;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.gennext.hiren.restconnect.RestClientHelper;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayMap<String, Object> postParams;
+    String URL="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        postParams = new ArrayMap<>();
+        postParams.put("id","252");
+        RestClientHelper.getInstance().post(URL, postParams, new RestClientHelper.RestClientListener() {
+            @Override
+            public void onSuccess(String response) {
+                Log.e("response",response);
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.e("response error",error);
+            }
+        });
+
+
     }
 }
